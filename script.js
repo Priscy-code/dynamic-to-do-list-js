c6a5664ad5d0451cb83fef84450e350144966a37
Priscy <priscillaakwetey03@gmail.com>
2024-07-21 19:11:41 +0000

    adding all

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
     const taskInput = document.getElementById("task-input")
     const taskList = document.getElementById("task-list")
 
-    function addTask(){
-        const taskText = taskInput.ariaValueMax.trim();
+    const addTask = function (){
+        const taskText = taskInput.value.trim();
         if (!taskText){
             alert("please enter a task");
             return;
