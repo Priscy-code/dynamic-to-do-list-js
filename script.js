caae928625b5b83608855ee0f573004678b966fa
Priscy <priscillaakwetey03@gmail.com>
2024-07-21 19:06:59 +0000

    adding all

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input")
+    const taskList = document.getElementById("task-list")
+
+    function addTask(){
+        const taskText = taskInput.ariaValueMax.trim();
+        if (!taskText){
+            alert("please enter a task");
+            return;
+        }
+        const newListItem = document.createElement("li");
+        newListItem.textContent = taskText;
+
+        const removeButton = document.createElement("button")
+        removeButton.textContent = "Remove";
+        removeButton.classList.add("remove-btn")
+
+        removeButton.addEventListener("click", function(){
+            taskList.removeChild(newListItem)})
+        
+        newListItem.appendChild(removeButton);
+        taskList.appendChild(newListItem);
+        
+    }
+    addButton.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+  addTask();
+})
